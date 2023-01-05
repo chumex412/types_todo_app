c13de5624fbe32ade120c3821af356ce32cc015d
Kode Navigator <chumex412@gmail.com>
2023-01-05 03:21:59 +0100

    adds todo, clears completed, deletes todo item and filters

--- build/app.js
+++ build/app.js
@@ -1,14 +1,112 @@
-var todoForm = document.querySelector(".add-todo-form");
-var todoListElem = document.querySelector('.todo-list');
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var todoList = [];
 document.addEventListener("DOMContentLoaded", function () {
-    togglerMode();
+    var mainWrapper = document.querySelector('.todo-main-wrapper');
+    var todoForm = document.querySelector(".add-todo-form");
+    var todoListElem = document.querySelector('.todo-list');
+    var clearCompletedBtn = document.querySelector('.clear-completed');
+    var modeToggleBtn = document.querySelector('.mode-toggler');
+    todoForm.addEventListener('submit', addTodo);
+    clearCompletedBtn.addEventListener('click', clearCompleted);
+    todoListElem.addEventListener('click', function (e) {
+        var target = e.target;
+        if (target.classList.contains("delete-todo-btn")) {
+            var id = target.dataset.id;
+            deleteTodoItem(id);
+        }
+    });
+    modeToggleBtn.addEventListener('click', togglerMode);
+    mainWrapper.addEventListener('click', function (e) {
+        var target = e.target;
+        if (target.classList.contains('filter-btn')) {
+            var name_1 = target.dataset.name;
+            filterTodo(name_1);
+        }
+    });
 });
 function togglerMode() {
 }
+function addTodo(e) {
+    e.preventDefault();
+    var todoName = document.querySelector('#todo-form-input');
+    var todoChecked = document.querySelector('#todo-form-check');
+    var todoItem = {
+        id: generateRandomId(),
+        name: todoName.value,
+        completed: todoChecked.checked
+    };
+    todoList.push(todoItem);
+    renderTodoList(todoList);
+}
 function renderTodoList(list) {
     var output = list.map(function (item) {
-        return "\n      <li class=\"todo-item\">\n        <input type=\"checkbox\" checked=".concat(item.completed, " name=\"").concat(item.name, "\" />\n        <p>").concat(item.name, "</p>\n        <button data-id=\"").concat(item.id, "\" class=\"delete-todo-btn\">&#10006;</button>\n      </li>\n    ");
+        return "\n      <li class=\"todo-item\">\n        <input type=\"checkbox\" ".concat(item.completed ? 'checked' : null, " name=\"").concat(item.name, "\" data-id=\"").concat(item.id, "\" />\n        <p style=\"text-decoration: ").concat(item.completed ? "line-through" : "none", ";\">").concat(item.name, "</p>\n        <button data-id=\"").concat(item.id, "\" class=\"delete-todo-btn\">&#10006;</button>\n      </li>\n    ");
     });
+    var todoListElem = document.querySelector('.todo-list');
     todoListElem.innerHTML = output.join("");
+    itemsLeftWatch(list);
+    var todoItemCheckboxes = document.querySelectorAll('.todo-item input[type="checkbox"]');
+    todoItemCheckboxes.forEach(function (todoItem) {
+        todoItem.addEventListener('change', function (e) {
+            var target = e.target;
+            var todoItemText = target.nextElementSibling;
+            var id = target.dataset.id;
+            var checked = target.checked;
+            if (checked) {
+                todoItemText.style.textDecoration = 'line-through';
+            }
+            else {
+                todoItemText.style.textDecoration = 'none';
+            }
+            todoList = todoList.map(function (item) {
+                if (item.id === id) {
+                    return __assign(__assign({}, item), { completed: checked });
+                }
+                return item;
+            });
+            itemsLeftWatch(todoList);
+        });
+    });
+}
+function deleteTodoItem(id) {
+    todoList = todoList.filter(function (item) { return item.id !== id; });
+    renderTodoList(todoList);
+}
+function clearCompleted() {
+    todoList = todoList.filter(function (item) { return !item.completed; });
+    renderTodoList(todoList);
+}
+function filterTodo(filter) {
+    var newTodoList = todoList.filter(function (item) {
+        if (filter === 'Active') {
+            return !item.completed;
+        }
+        else if (filter === 'Completed') {
+            return item.completed;
+        }
+        else {
+            return item;
+        }
+    });
+    renderTodoList(newTodoList);
+}
+function itemsLeftWatch(list) {
+    var remainingTaskElem = document.querySelector('.todo-bottom > p');
+    var count = list.filter(function (item) { return !item.completed; }).length;
+    var remainingTaskText = "\n    ".concat(count === 0 ? 'No tasks left or added' : count === 1 ? count + " item left" : count + " items left", "\n  ");
+    remainingTaskElem.innerHTML = remainingTaskText;
+}
+function generateRandomId() {
+    return Date.now().toString(20) + Math.random().toString(20).substring(2);
 }
 //# sourceMappingURL=app.js.map
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,7 +2,7 @@
 
 // Type declarations
 
-type Primitives = string | number | boolean | null
+type Primitives = string | number | boolean | null;
 
 type TodoObject = {
   id: string
@@ -10,30 +10,150 @@ type TodoObject = {
   completed: boolean
 }
 
-// DOM selectors
-const todoForm = document.querySelector(".add-todo-form") as HTMLFormElement;
-const todoListElem = document.querySelector('.todo-list') as HTMLElement;
+type TodoArray = TodoObject[];
+
+let todoList: TodoArray = []
 
 document.addEventListener("DOMContentLoaded", function(): void {
-  togglerMode()
+  // DOM selectors
+  const mainWrapper = document.querySelector('.todo-main-wrapper') as HTMLElement;
+  const todoForm = document.querySelector(".add-todo-form") as HTMLFormElement;
+  const todoListElem = document.querySelector('.todo-list') as HTMLElement;
+  const clearCompletedBtn = document.querySelector('.clear-completed') as HTMLButtonElement;
+  const modeToggleBtn = document.querySelector('.mode-toggler') as HTMLButtonElement;
+  
+  // Events
+  todoForm.addEventListener('submit', addTodo)
+  clearCompletedBtn.addEventListener('click', clearCompleted)
+  todoListElem.addEventListener('click', function(e: Event): void {
+    const target = <HTMLElement>e.target;
+
+    if (target.classList.contains("delete-todo-btn")) {
+      const id: string = target.dataset.id;
+
+      deleteTodoItem(id)
+    }
+  })
+  
+  modeToggleBtn.addEventListener('click', togglerMode)
+  mainWrapper.addEventListener('click', function(e: Event): void {
+    const target = <HTMLButtonElement> e.target;
+
+    if(target.classList.contains('filter-btn')) {
+      const name: string = target.dataset.name;
+
+      filterTodo(name)
+    }
+  })
 })
 
 function togglerMode(): void {
 
 }
 
-function renderTodoList(list: TodoObject[]): void {
+function addTodo(e: Event): void {
+  e.preventDefault()
+
+  const todoName = document.querySelector('#todo-form-input') as HTMLInputElement;
+  const todoChecked = document.querySelector('#todo-form-check') as HTMLInputElement;
+
+  const todoItem: TodoObject = {
+    id: generateRandomId(),
+    name: todoName.value,
+    completed: todoChecked.checked
+  }
+  
+  todoList.push(todoItem)
+
+  renderTodoList(todoList)
+}
+
+function renderTodoList(list: TodoArray): void {
   const output: string[] = list.map((item: TodoObject) => {
 
     return `
       <li class="todo-item">
-        <input type="checkbox" checked=${item.completed} name="${item.name}" />
-        <p>${item.name}</p>
+        <input type="checkbox" ${item.completed ? 'checked': null} name="${item.name}" data-id="${item.id}" />
+        <p style="text-decoration: ${item.completed ? "line-through" : "none"};">${item.name}</p>
         <button data-id="${item.id}" class="delete-todo-btn">&#10006;</button>
       </li>
     `
   })
 
+  const todoListElem = document.querySelector('.todo-list') as HTMLElement;
+
   todoListElem.innerHTML = output.join("");
 
+  itemsLeftWatch(list)
+
+  let todoItemCheckboxes = <NodeList> document.querySelectorAll('.todo-item input[type="checkbox"]')
+
+  todoItemCheckboxes.forEach((todoItem) => {
+    todoItem.addEventListener('change', function(e: Event): void {
+      const target = <HTMLInputElement> e.target;
+      const todoItemText = <HTMLParagraphElement> target.nextElementSibling;
+
+      const id: string = target.dataset.id;
+
+      const checked: boolean = target.checked
+
+      if(checked) {
+        todoItemText.style.textDecoration = 'line-through';
+      } else {
+        todoItemText.style.textDecoration = 'none';
+      }
+
+      todoList = todoList.map(item => {
+        if(item.id === id) {
+          return { ...item, completed: checked }
+        }
+
+        return item
+      })
+
+      itemsLeftWatch(todoList)
+    })
+    
+  })
+}
+
+function deleteTodoItem(id: string): void {
+  todoList = todoList.filter((item) => item.id !== id)  
+
+  renderTodoList(todoList)
+}
+
+function clearCompleted(): void {
+  todoList = todoList.filter((item) => !item.completed)  
+
+  renderTodoList(todoList)
+}
+
+function filterTodo(filter: string): void {
+  const newTodoList: TodoArray = todoList.filter(item => {
+    if(filter === 'Active') {
+      return !item.completed
+    } else if (filter === 'Completed') {
+      return item.completed
+    } else {
+      return item
+    }
+  })
+
+  renderTodoList(newTodoList)
+}
+
+function itemsLeftWatch(list: TodoArray): void {
+  const remainingTaskElem = <HTMLParagraphElement> document.querySelector('.todo-bottom > p');
+
+  const count: number = list.filter(item => !item.completed).length;
+
+  const remainingTaskText: string = `
+    ${count === 0 ? 'No tasks left or added' : count === 1 ? count + " item left" : count + " items left"}
+  `
+  remainingTaskElem.innerHTML = remainingTaskText;
+}
+
+function generateRandomId (): string {
+  return Date.now().toString(20) + Math.random().toString(20).substring(2);
 }
